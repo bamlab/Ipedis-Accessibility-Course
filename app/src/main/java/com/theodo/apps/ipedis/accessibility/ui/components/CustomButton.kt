43c2ac7eb0473be72e7f259cf90dab5f42f80fd0
Dennis BORDET <dennisb@bam.tech>
2024-11-12 14:48:37 +0100

    feat: customIconButton.kt

--- app/src/main/java/com/theodo/apps/ipedis/accessibility/ui/components/CustomButton.kt
+++ app/src/main/java/com/theodo/apps/ipedis/accessibility/ui/components/CustomButton.kt
@@ -1,7 +1,6 @@
 package com.theodo.apps.ipedis.accessibility.ui.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,7 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.theodo.apps.ipedis.accessibility.ui.theme.IpedisAndroidAccessibilityCourseTheme
@@ -21,36 +19,32 @@ import com.theodo.apps.ipedis.accessibility.ui.theme.Purple40
 fun CustomButton(
     text: String,
     onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     Text(
         text = text,
         color = Color.White,
-        modifier = modifier
-            .clip(RoundedCornerShape(50))
-            .background(Purple40)
-            .padding(8.dp)
-            .padding(horizontal = 12.dp)
-            .pointerInput("") {
-                this.detectTapGestures {
-                    onClick()
-                }
-            }
+        modifier =
+            modifier
+                .clip(RoundedCornerShape(50))
+                .background(Purple40)
+                .padding(8.dp)
+                .padding(horizontal = 12.dp)
+                .customClick(onClick),
     )
 }
 
-
 @Preview
 @Composable
-private fun CustomButtonPreview() = IpedisAndroidAccessibilityCourseTheme {
-    Column {
-        CustomButton(
-            text = "toto",
-            onClick = {},
-        )
-        Button(onClick = {}) {
-            Text("toto")
+private fun CustomButtonPreview() =
+    IpedisAndroidAccessibilityCourseTheme {
+        Column {
+            CustomButton(
+                text = "toto",
+                onClick = {},
+            )
+            Button(onClick = {}) {
+                Text("toto")
+            }
         }
-
     }
-}
---
+++ app/src/main/java/com/theodo/apps/ipedis/accessibility/ui/components/CustomIconButton.kt
@@ -0,0 +1,42 @@
+package com.theodo.apps.ipedis.accessibility.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.theodo.apps.ipedis.accessibility.ui.theme.IpedisAndroidAccessibilityCourseTheme
+
+@Composable
+fun CustomIconButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    description: String? = null,
+    image: ImageVector,
+) {
+    Image(
+        imageVector = image,
+        contentDescription = description,
+        modifier =
+            modifier
+                .size(48.dp)
+                .padding(12.dp)
+                .customClick(onClick),
+    )
+}
+
+@Preview
+@Composable
+private fun CustomIconButtonPreview() =
+    IpedisAndroidAccessibilityCourseTheme {
+        CustomIconButton(
+            onClick = {},
+            description = "Favorite",
+            image = Icons.Default.Favorite,
+        )
+    }
